6e619a893fc9bccd5c9ad2d04684dae508ba243f
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2023-08-15 22:20:19 -0300

    Update exercicio2.js

--- exercicio2.js
+++ exercicio2.js
@@ -1,3 +1,10 @@
+/*Pergunta:
+Um robô deve caminhar sobre um tabuleiro quadriculado. As dimensões do tabuleiro são 10 x 10 metros e cada quadriculado, 1 x 1 metro.
+Elabore uma classe que represente o robô e os seus movimentos, considerando que:
+- Os movimentos do robô não podem ultrapassar os limites do tabuleiro;
+- O robô terá dois tipos de movimentos: andar para frente, sendo 1 passo por vez e girar no sentido horário 90 graus.
+- O robô deverá sinalizar quando ele atingir qualquer limite do tabuleiro.
+Escolha um ponto de partida quando o robô for instanciado para iniciar os movimentos.*/
 class Robo{
     constructor(frente, direita, esquerda){
         this.frente= frente;
