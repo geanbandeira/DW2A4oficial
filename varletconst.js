30e06b2c4be9c0307fc47baa4816baad4469f570
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2023-08-22 22:21:03 -0300

    Add files via upload

---
+++ varletconst.js
@@ -0,0 +1,40 @@
+/*
+let idade = 19;
+if(idade >17){
+    console.log("Pode curtir");
+}else{
+    console.log("volta pra casa já");
+}
+*/
+
+
+//error let
+let idade = 15;
+if(idade>14){
+    console.log("Pode jogar");
+}else{
+    console.log("Sem acesso")
+}
+idade=20;
+
+/*const Dia = 6;
+console.log("O dia no meu niver no Dia " + Dia);*/
+
+
+//erro const
+/*const Mes = 6;
+Mes = 12;
+*/
+/*
+for (var ano= 2023; ano <=2030; ano++){
+    console.log(ano);
+}
+*/
+
+//error var
+/*
+var x = 30;{
+    var x = 900;
+}
+console.log(x);
+*/
